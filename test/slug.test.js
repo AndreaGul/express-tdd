ad31f71fb2c5c29c88e8ffa56b4d4368344895e7
AndreaGul <andreagulino99@gmail.com>
2024-05-30 16:02:35 +0200

    feat: es 1 createSlug dovrebbe ritornare una stringa

---
+++ test/slug.test.js
@@ -0,0 +1,31 @@
+const { test, expect } = require("@jest/globals");
+
+const posts = require('../posts');
+
+const creaSlug = (name) => {
+    const baseSlug = name.replaceAll(' ', '-').toLowerCase().replaceAll('/', '');
+    const slugs = pizze.map(p => p.slug);
+    let counter = 1;
+    let slug = baseSlug;
+    while(slugs.includes(slug)){
+        slug = `${baseSlug}-${counter}`;
+        counter ++;
+    }
+    return slug;
+}
+
+const createSlug = (titolo) =>{
+    if(typeof titolo !== 'string'){
+         throw new Error("Il titolo passato non è una stringa");
+     }
+    const baseSlug = titolo.replaceAll(' ','-').toLowerCase().replaceAll('/', '');
+
+    return baseSlug ;
+}
+
+// createSlug dovrebbe ritornare una stringa
+test('createSlug dovrebbe ritornare una stringa',()=>{
+    const slug = createSlug("questa e una stringa");
+
+    expect(()=>{slug= 10}).toThrow();
+})
